a88f496cdcdec883ca4973851ce21755ce7f7406
ruhul005 <ruhulamin@Ruhuls-MacBook-Pro.local>
2017-11-26 11:11:10 +0600

    Binary

---
+++ BinarySearchTree/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: ruhulamin
+ *
+ * Created on November 26, 2017, 9:44 AM
+ */
+
+#include <iostream>
+#include"binarysearchtree.cpp"
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    TreeType <int> myTree;
+    cout<<"An Empty tree has been created"<<endl;
+    return 0;
+}
+
